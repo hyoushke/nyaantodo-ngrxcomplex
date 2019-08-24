b2d97323c733d6c6998958a8dc03045e2ce78d66
hyoushke <hyoushke@gmail.com>
2019-08-24 03:13:26 -0700

    ng g component components/todo/todo-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoContainerComponent } from './components/todo/todo-container/todo-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoContainerComponent
   ],
   imports: [
     BrowserModule,
